a3bfdd207ba632d17a95c4e88081b0c8d3025cab
zoricasebalj <zorica.sebalj5@gmail.com>
2024-05-25 10:32:59 +0200

    skripta2

--- wellness.sql
+++ wellness.sql
@@ -22,7 +22,7 @@ zaposlenik int);
 create table usluge(
 sifra int not null primary key identity(1,1),
 vrstausluge varchar(50)not null,
-trajanje datetime not null,
+trajanje int not null,
 cijena decimal(18,2)not null,
 );
 create table zaposlenici(
@@ -31,3 +31,30 @@ ime varchar(20)not null,
 prezime varchar(30)not null,
 strucnost varchar(50),
 );
+
+alter table termini add foreign key (klijent) references klijenti(sifra);
+alter table termini add foreign key (usluga) references usluge(sifra);
+alter table termini add foreign key (zaposlenik) references zaposlenici(sifra);
+
+
+insert into klijenti(ime,prezime,email)
+values ('Ivan','Horvat','ivanhorvat@gmail.com');
+
+
+
+
+insert into usluge(vrstausluge,trajanje,cijena)
+values ('masaža',1,49.99);
+
+insert into zaposlenici(ime,prezime,strucnost)
+values ('Pero','Perić','fizioterapetut');
+
+insert into termini(datum,klijent,usluga,zaposlenik)
+values ('2024-05-21 16:00',1,'1','1');
+
+select* from klijenti;
+select* from usluge;
+select*from zaposlenici;
+select*from termini;
+
+
